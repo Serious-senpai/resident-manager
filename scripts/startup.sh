721ce7fb43a6a966fe8de0363d774f71812b6475
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-09-16 17:56:12 +0700

    Configure deployment settings

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,3 @@
 #! https://stackoverflow.com/a/246128
-SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
-ROOT_DIR=$(realpath $SCRIPT_DIR/..)
-
-pip install -r $ROOT_DIR/requirements.txt
-python $ROOT_DIR/main.py
+pip install -r $DEPLOYMENT_SOURCE/requirements.txt
+python $DEPLOYMENT_SOURCE/main.py
