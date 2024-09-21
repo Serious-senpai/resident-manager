b3dcb8fdcd7caa4b76ec9e0f248c742b41b60a2b
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-09-21 20:14:18 +0700

    Minor API changes

--- server/routes/api/admin/login.py
+++ server/routes/api/admin/login.py
@@ -9,17 +9,21 @@ from ....models import Authorization
 from ....routers import api_router
 
 
+__success_status = status.HTTP_204_NO_CONTENT
+__failure_status = status.HTTP_403_FORBIDDEN
+
+
 @api_router.post(
     "/admin/login",
     name="Administrators login",
     tags=["authorization", "admin"],
     response_model=None,
-    responses={status.HTTP_403_FORBIDDEN: {}},
-    status_code=status.HTTP_204_NO_CONTENT,
+    responses={__failure_status: {}},
+    status_code=__success_status,
 )
 async def admin_login(headers: Annotated[Authorization, Header()]) -> None:
-    """Verify administrator authorization data, return 204 on success, 403 on failure"""
+    f"""Verify administrator authorization data, return {__success_status} on success, {__failure_status} on failure"""
     if await Database.instance.verify_admin(headers.username, headers.password):
         return None
 
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    raise HTTPException(status_code=__failure_status)
