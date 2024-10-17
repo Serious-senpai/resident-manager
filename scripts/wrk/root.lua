7462ed8d68840c94ba3bc7848d2d3f7fe2dae86e
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-10-17 15:15:26 +0000

    Pre-generate requests before performance benchmarking

--- scripts/wrk/root.lua
+++ scripts/wrk/root.lua
@@ -1,2 +1,19 @@
 wrk.path = "/"
 wrk.method = "GET"
+
+local request
+
+function init(args)
+    -- Pre-generate the request
+    -- Usage of \r\n: RFC2616 explicitly stated that header lines must end with a CRLF sequence
+    request = string.format("%s %s HTTP/1.1\r\n", wrk.method, wrk.path) ..
+        "Host: localhost:8000\r\n" ..
+        "Connection: keep-alive\r\n" ..
+        "\r\n"
+
+    response = nil
+end
+
+function request()
+    return request
+end
