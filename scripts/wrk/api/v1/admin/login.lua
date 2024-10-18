9f250a18f4bd817cb6f960febe070424d6e9f339
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-10-18 04:43:24 +0000

    Generate random User-Agent in wrk

--- scripts/wrk/api/v1/admin/login.lua
+++ scripts/wrk/api/v1/admin/login.lua
@@ -1,4 +1,16 @@
-wrk.path = "/api/v1/admin/login"
+local utils = require("utils")
+
+local user_agent = utils.random_user_agent()
+print("User-Agent: " .. user_agent)
+
+wrk.scheme = "http"
+wrk.host = "localhost"
+wrk.port = 8000
 wrk.method = "POST"
-wrk.headers["username"] = "incorrect"
-wrk.headers["password"] = "incorrect"
+wrk.path = "/api/v1/admin/login"
+wrk.headers = {
+    ["Username"] = "incorrect",
+    ["Password"] = "incorrect",    
+    ["User-Agent"] = user_agent,
+}
+wrk.body = nil
--- scripts/wrk/root.lua
+++ scripts/wrk/root.lua
@@ -1,2 +1,14 @@
-wrk.path = "/"
+local utils = require("utils")
+
+local user_agent = utils.random_user_agent()
+print("User-Agent: " .. user_agent)
+
+wrk.scheme = "http"
+wrk.host = "localhost"
+wrk.port = 8000
 wrk.method = "GET"
+wrk.path = "/"
+wrk.headers = {
+    ["User-Agent"] = user_agent,
+}
+wrk.body = nil
